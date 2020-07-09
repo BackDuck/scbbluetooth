bc904255a26802d593c9677aab0fff3472e35f6b
Nikita Khokhlov <nkhokhlov11708@gmail.com>
2020-07-09 18:42:35 +0300

    Screens design

---
+++ app/src/main/java/com/example/scbbluetooth/presentation/ui/WorkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scbbluetooth.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scbbluetooth.R
+
+class WorkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work)
+    }
+}
